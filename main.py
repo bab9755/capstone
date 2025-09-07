db906ac10cffbba106e260f4d526e1785a0e50a0
Boubacar Ballo <bab9755@nyu.edu>
2025-09-07 22:42:11 +0400

    update simulation

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from vi import Agent, Config, Simulation
+from vi.util import count
+import random
+
+class myAgent(Agent):
+    def update(self) -> None:
+        if count(self.in_proximity_accuracy()) >= 3:
+            self.change_image(0)
+            self.freeze_movement()
+            print("Freezing movement")
+        else:
+            self.change_image(1)
+            self.continue_movement()
 
 
-class myAgent(Agent):...
 (
     Simulation(Config())
     .batch_spawn_agents(100, myAgent, images=["/Users/boubalkaly/Desktop/development/capstone/test-violet/images/green.png", "/Users/boubalkaly/Desktop/development/capstone/test-violet/images/red.png"])

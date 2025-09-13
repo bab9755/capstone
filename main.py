407167242a5eba6ffd8e8b4bd945ff74efb17dae
Boubacar Ballo <bab9755@nyu.edu>
2025-09-13 17:21:10 +0400

    udpate main experiemnt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from vi import Agent, Config, Simulation
+from vi import Agent, Config, Simulation, Window
 from vi.util import count
 import random
 
@@ -12,9 +12,12 @@ class myAgent(Agent):
             self.change_image(1)
             self.continue_movement()
 
-
+config = Config(window=Window(1000, 700))
+x, y = config.window.as_tuple()
 (
-    Simulation(Config())
+    Simulation(config)
+    .spawn_obstacle("images/triangle.png", x//2, y//2)
+    .spawn_site("images/obstacle.png", x=375, y=375)
     .batch_spawn_agents(100, myAgent, images=["/Users/boubalkaly/Desktop/development/capstone/test-violet/images/green.png", "/Users/boubalkaly/Desktop/development/capstone/test-violet/images/red.png"])
     .run()
 )

ba793b6d9d8ec7a3f2284844cfcd3304b05dd796
Boubacar Ballo <bab9755@nyu.edu>
2025-09-16 12:04:51 +0400

    test env setup

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from vi import Agent, Config, Simulation, Window
 from vi.util import count
 import random
-
+from environment import build_environment, Environment, ItemSpec, WorldConfig
 class myAgent(Agent):
     def update(self) -> None:
-        self.check_site()
+        self.check_obstacle()
 
-    def check_site(self):
-        if self.on_site():
-            site_id = self.on_site_id()
-            print(f"Agent with id {self.id} is on site {site_id}")
+    def check_obstacle(self):
+        if count(self.obstacle_intersections()) >= 1:
+            print(f"Agent with id {self.id} is on obstacle")
             return
 
 config = Config(window=Window(1000, 700))
 x, y = config.window.as_tuple()
+
+environment = Environment(seed=1, world=WorldConfig(width=x, height=y), obstacles=[ItemSpec(image="images/triangle.png", count=20)], sites=[ItemSpec(image="images/red.png", count=1)])
+env = build_environment(environment)
+
+simulation = Simulation(config)
+
+for ob in env["obstacles"]:
+    simulation.spawn_obstacle(ob["image"], ob["x"], ob["y"])
+for site in env["sites"]:
+    simulation.spawn_site(site["image"], x=site["x"], y=site["y"])
+
 (
-    Simulation(config)
+    simulation
     .spawn_obstacle("images/triangle.png", x//2, y//2)
     .spawn_site("images/triangle.png", x=random.randint(0, x), y=random.randint(0, y))
     .spawn_site("images/triangle.png", x=random.randint(0, x), y=random.randint(0, y))

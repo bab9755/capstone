c6e2ca32f597e23b4982ffa3fde8a32f0c16c429
Boubacar Ballo <bab9755@nyu.edu>
2025-09-18 23:20:36 +0400

    update sensors

--- sensors.py
+++ sensors.py
@@ -1,15 +1,17 @@
 from vi.util import Vector2
 from vi.simulation import Shared
+from constants import WIDTH, HEIGHT
 class Sensor:
 
     def __init__(self, agent):
         self.agent = agent
 
-    def get_position(self) ->Vector2:
-        return self.agent.pos #returns the position of the agent
+    def border_collision(self):
+        pos = self.agent.pos
 
-    def get_next_move(self) -> Vector2:
-        return self.agent.move #return the delta of the next move e.g (2, 1) 2 pixels on x and 1 on y
+        if pos.x < 0 or pos.x > WIDTH or pos.y < 0 or pos.y > HEIGHT:
+            return True
+        return False
 
 
 
@@ -17,24 +19,17 @@ class Actuator: #this is what we are going to use to move the agent
 
     def __init__(self, agent):
         self.agent = agent
+        self.agent.current_angle = random.uniform(0, 360)
 
-    def move(self, move: Vector2):
-        self.agent.move = move 
 
-    def change_image(self, image: str):
-        self.agent.image = image 
+
+    def update_position(self, linear_speed: int, angular_velocity: int): # this should run at every step
+
+        self.agent.current_angle += angular_velocity
+        self.agent.current_angle %= 360
+
+        self.agent.move.from_polar((linear_speed, self.agent.current_angle)) #convert from polar to cartesian
+
+        self.agent.pos += self.agent.move
         
-
-    def update_position(self, angular_velocity: Vector2, linear_velocity: Vector2): # this should run at every step
-        # Use simulator delta time to integrate motion
-        delta_time = getattr(self.agent.shared, "delta_time", 0.0) # but then each delta time is equal to 1 step
-
-        # Update position using current orientation vector `move`
-        if linear_velocity:
-            self.agent.pos += self.agent.move * (linear_velocity * delta_time)
-
-        # Update orientation by rotating the orientation vector by the incremental angle
-        if angular_velocity:
-            incremental_angle = angular_velocity * delta_time
-            # Rotate in-place to preserve vector magnitude
-            self.agent.move.rotate_ip(incremental_angle)
+        

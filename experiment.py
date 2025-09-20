7c52e1020a00e2931a680857582aad596fbed009
Boubacar Ballo <bab9755@nyu.edu>
2025-09-20 19:59:48 +0400

    udpate experiment

--- experiment.py
+++ experiment.py
@@ -3,7 +3,7 @@ from vi import Config, Simulation, Window
 from environment import Environment
 from constants import WIDTH, HEIGHT
 
-config = Config(window=Window(WIDTH, HEIGHT), movement_speed=1, seed=3)
+config = Config(window=Window(WIDTH, HEIGHT),seed=3)
 simulation = Environment(config)
-simulation.batch_spawn_agents(10, knowledgeAgent, images=["images/red.png"])
+simulation.batch_spawn_agents(2, knowledgeAgent, images=["images/red.png"])
 simulation.run()

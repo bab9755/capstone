f0abff38f0c873154fb35a831d2f181c24467511
Boubacar Ballo <bab9755@nyu.edu>
2025-09-21 21:04:27 +0400

    add communication manager to simulation

--- environment.py
+++ environment.py
@@ -1,9 +1,10 @@
 from vi import Agent, Config, Simulation, Window
-
+from communication import CommunicationManager
 
 class Environment(Simulation):
     def __init__(self, config = None):
         super().__init__(config)
+        self.communication_manager = CommunicationManager()
 
     def _HeadlessSimulation__update_positions(self):
         for sprite in self._agents.sprites():
